3b29ae01b26dc90647c238908e5c77715970013d
Ayush Dhamankar <145098838+ayush-vcet@users.noreply.github.com>
2023-10-09 11:09:06 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# Open Complaint Network
---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import {useState} from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+import Register from './Components/Register/Register';
+import Check from './Components/Check/Check';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+
+function App() {
+  const [state, setState] = useState({
+    web3:null,
+    contract:null
+  })
+  const [acc, setacc] = useState("");
+  const saveState = (state, account) => {
+    console.log(state);
+    setState(state);
+    setacc(account);
+    console.log(account);
+  }
+  return (
+    <>
+    <Router>
+        <Navbar saveState={saveState}></Navbar>
+           <Routes>
+                <Route exact path='/' element={< Hero />}></Route>
+                <Route exact path='/register' element={< Register state={state} acc={acc} />}></Route>
+                <Route exact path='/check' element={< Check state={state} />}></Route>
+          </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
